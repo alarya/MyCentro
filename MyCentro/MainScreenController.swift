fcec1f8a1bb227c32da29635647e71a777c1f4bc
Alok Arya <alarya@lcs-208-171.syr.edu>
2015-11-18 14:50:09 -0500

    segue to Bus List

--- MyCentro/MainScreenController.swift
+++ MyCentro/MainScreenController.swift
@@ -11,26 +11,48 @@ import UIKit
 
 class MainScreenController: UIViewController {
     
-    override func viewDidLoad() {
+    @IBOutlet weak var busToHomeButton: UIButton!
+    
+    @IBOutlet weak var busToWorkButton: UIButton!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func busToHome(sender: UIButton) {
+    @IBAction func busToHome(sender: UIButton)
+    {
+        performSegueWithIdentifier("BusListToHome", sender: sender);
     }
     
-    @IBAction func busToWork(sender: UIButton) {
+    @IBAction func busToWork(sender: UIButton)
+    {
+        performSegueWithIdentifier("BusListToWork", sender: sender);
     }
     
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == ""
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
+    {
+        if segue.identifier == "BusListToHome"
         {
-            segue.destinationViewController;
+            if let destinationVC = segue.destinationViewController as? BusListController
+            {
+                destinationVC.title = "Bus to Home";
+            }
+        }
+        else if segue.identifier == "BusListToWork"
+        {
+            if let destinationVC = segue.destinationViewController as? BusListController
+            {
+                destinationVC.title = "Bus to Work";
+            }
         }
     }
 }
